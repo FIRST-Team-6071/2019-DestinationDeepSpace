fceac7c758936525921491b3e71b2eb089a02be6
Zachary Seebeck <seebeckz@live.com>
2019-01-09 22:31:17 -0500

    Added in comments to help new students understand the code.

--- Daedalus/src/main/java/frc/robot/RobotMap.java
+++ Daedalus/src/main/java/frc/robot/RobotMap.java
@@ -21,14 +21,26 @@ public class RobotMap{
             System.out.println("Created Drivetrain.");
         }
 
+        /**
+         * Set the speeds of the left and right sides of the robot.
+         * 
+         * We use 2 motors on each side, but we use a PWM splitter going from one port on the RoboRIO to 2 seperate Rev Robotics Spark motor controllers. Hence why we are able to have just the 2 controllers.
+         * 
+         * @param leftAmmount
+         * @param rightAmmount
+         */
         public void Move(double leftAmmount, double rightAmmount) {
             rightDrive.setInverted(true);
-            
+
+            // TODO: Implement Exponential Drive.
             leftDrive.set(leftAmmount);
             rightDrive.set(rightAmmount);
 
         }
 
+        /**
+         * Stop all motors on the robot.
+         */
         public void Stop() {
             leftDrive.stopMotor();
             rightDrive.stopMotor();
@@ -44,17 +56,30 @@ public class RobotMap{
 
     }
 
+
+    /**
+     * Currently, this does nothing. It's just a place holder for when we get around to putting together the ramp.
+     * 
+     */
     public class Ramp {
+        
+        void Ramp() {
+            throw new UnsupportedOperationException("No ramp code currently availiable.");
+        }
 
     }
 
 
     /**
      * The class for receiving input from the joysticks in Driver Station.
+     * 
+     * We use a dual Joystick configuration for more percise control of the robot. 
      */
     public class Input {
         Joystick leftJoy = new Joystick(0);
         Joystick rightJoy = new Joystick(1);
+        
+
         
     }
 
